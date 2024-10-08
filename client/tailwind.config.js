3e18d7d81c545f9320e2d216199adddcc843178d
vjk-2k5 <svijaykrishna12@gmail.com>
2024-10-09 02:03:27 +0530

    fixing fonts

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -58,7 +58,7 @@ module.exports = {
         sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        sans: ['Inter', 'sans-serif'],
       },
       keyframes: {
         "accordion-down": {
