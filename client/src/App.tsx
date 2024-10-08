ccc4167f2301e48e865a59b43224cd54d38d18c2
vjk-2k5 <svijaykrishna12@gmail.com>
2024-10-09 03:52:37 +0530

    Upload image and preview

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 import './App.css';
 import { Button } from './components/Button'; // Adjust the import path if necessary
 import { MainLayout } from "./layouts/MainLayout";
-
+import { MainContent } from "./components/MainContent";
 const App = () => {
   return (
     <MainLayout>
-      <h2 className="text-2xl font-bold mb-4">Welcome to the Dashboard</h2>
-      <p>This is your main content area. You can add your page content here.</p>
+      <MainContent />
     </MainLayout>
   );
 };
