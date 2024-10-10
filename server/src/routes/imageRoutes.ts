ffa008be08069094d8d77460eea53447701da0d5
rohit <rohit.selvakumaran@gmail.com>
2024-10-11 03:50:31 +0530

    final changes in backend

--- server/src/routes/imageRoutes.ts
+++ server/src/routes/imageRoutes.ts
@@ -7,4 +7,5 @@ const router = Router();
 router.post('/upload', upload.single('image'), uploadImage);
 router.get('/results', getResults);
 router.get('/results/:id',getResultsById);
+
 export default router;
